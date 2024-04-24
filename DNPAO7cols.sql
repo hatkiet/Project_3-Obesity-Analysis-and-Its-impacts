848aa44631ffa9e1db19e64175fd32029af958b5
mmacinnis <michaelgmacinnis@gmail.com>
2024-04-23 21:13:58 -0400

    Add files via upload

---
+++ DNPAO7cols.sql
@@ -0,0 +1,18 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/AgE2nU
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "DNPAO" (
+    "YearStart" INT,
+    "LocationAbbr" VARCHAR,
+    "LocationDesc" VARCHAR,
+    "Data_Value" FLOAT,
+    "QuestionID" VARCHAR,
+    "Stratification1" VARCHAR,
+    "StratificationCategoryId1" VARCHAR
+);
+
